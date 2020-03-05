ddce33011bf3e18ec1fdceb4f0daeb124cd1b0b8
Varun Dodla <varun@dodla.com>
2020-03-06 03:55:09 +0530

    add new method to create contacts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@
 import Koa from 'koa'
 const app = new Koa();
 
+// http body parser
+import bodyparser from 'koa-bodyparser'
+app.use(bodyparser())
+
 // Database
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 const adapter = new FileSync('res/db.json')
 const db = low(adapter)
+
+interface IContact {
+  id: number
+  firstname: string
+  lastname: string
+  phone: string
+  email: string
+}
 
 // Templates
 import Pug from 'koa-pug'
@@ -27,6 +39,27 @@ router.get('/', async (ctx) => {
 
 router.post('/:id/delete', async (ctx) => {
   db.get('contacts').remove({ id: +ctx.params.id }).write()
+  await ctx.redirect('/')
+});
+
+router.post('/', async (ctx) => {
+  // get all contacts
+  const contacts: IContact[] = db.get('contacts').value()
+
+  // get the max id
+  const maxId = contacts.reduce((prev, curr) => {
+    return Math.max(prev, curr.id)
+  }, 0)
+
+  // create new contact
+  contacts.push({
+    ...ctx.request.body,
+    id: maxId + 1
+  })
+
+  // write to db
+  db.setState({ contacts }).write()
+  
   await ctx.redirect('/')
 });
 
