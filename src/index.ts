1dc45889f410909a3cce8cc72ad39b4a100493fc
Varun Dodla <varun@dodla.com>
2020-03-06 00:55:52 +0530

    add basic server hello world

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Koa from 'koa'
+
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+});
